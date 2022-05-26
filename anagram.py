36ce9ddc050b8941f21e13b4c369ab1f24951e0d
tuyojr <85107972+tuyojr@users.noreply.github.com>
2022-05-26 18:03:08 +0100

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word1, word2):
+    # [assignment] Add your code here
+
+    if(sorted(word1) == sorted(word2)):
+        print(sorted(word1), sorted(word2))
+        return True
+    return False
+
+print(find_anagram("listen", "silent"))
+
+
+#submitted by Adedolapo Olutuyo I4G000435EAL
+
